24c82ce8dc5804bb57dbe9820ddc4d5181d240ee
AbdinasirM <aabdi5520@gmail.com>
2025-05-28 22:40:34 -0500

    Nothing much. just simple jwt

--- Backend/Api/helpers/jtw_generation.py
+++ Backend/Api/helpers/jtw_generation.py
@@ -1,1 +1,20 @@
 #returns a payload for authentication
+import jwt
+
+#save this into .env
+secret = "test"
+
+payload_data = {
+    "sub": "4242",
+    "name": "Jessica Temporal",
+    "nickname": "Jess"
+}
+def encode_jwt(payload_data):    
+    # Create the token
+    token = jwt.encode(payload_data, my_secret, algorithm='HS256')
+    print("JWT Token:", token)
+
+
+def decode_jwt(encoded_token):
+    decoded = jwt.decode(encoded_token,  key='my_super_secret', algorithms=['HS256'])
+    print("Decoded Payload:", decoded)
